1dd9ec06481f343b355c38bced65bb3a24b3e891
Nathan Townsend <nathan@nathantownsend.com>
2014-04-22 07:09:11 -0600

    Site Actions Panel
    Started creating the site actions panel

---
+++ SBO/App_Start/Bundles/SBOBundles.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Optimization;
+
+namespace SBO
+{
+    public static class SBOBundles
+    {
+        public static Bundle Styles()
+        {
+            return new StyleBundle("~/Content/sbo/css").Include(
+                       "~/Content/Admin.css"
+                       );
+        }
+
+        public static Bundle Scripts()
+        {
+            return new ScriptBundle("~/bundles/sbo").Include(
+                      "~/Scripts/sbo/navigation.js"
+                      );
+        }
+    }
+}
