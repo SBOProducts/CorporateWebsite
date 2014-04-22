5c6343fd7cf57884d68a76973a886390edfeaa2c
Nathan Townsend <ntownsend2@mt.gov>
2014-04-22 16:19:00 -0600

    Added Website Area

---
+++ SBO/Areas/Website/WebsiteAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace SBO.Areas.Website
+{
+    public class WebsiteAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Website";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Website_default",
+                "Website/{customer}/{controller}/{action}/{id}",
+                new {area="Website", customer="Demo", controller="Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "SBO.Areas.Website.Controllers" }
+            );
+        }
+    }
+}
